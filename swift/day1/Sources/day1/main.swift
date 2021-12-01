f7e87245e6b7785d2be43830dc224a04cde48a5d
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-01 22:24:29 +0100

    Add solution for second part

--- swift/day1/Sources/day1/main.swift
+++ swift/day1/Sources/day1/main.swift
@@ -11,4 +11,21 @@ let numberOfIncreases = lines.adjacentPairs().map { $0 < $1 }
     .filter { $0 == true }
     .count
 
-print(numberOfIncreases)
+print("Part 1: \(numberOfIncreases)")
+
+extension Sequence where Element: Numeric {
+    /// Returns the sum of all elements in the collection
+    func sum() -> Element { return reduce(0, +) }
+}
+
+let reducedWindows = lines
+    .windows(ofCount: 3)
+    .map { $0.sum() }
+
+let result = reducedWindows
+    .adjacentPairs()
+    .map { $0 < $1 }
+    .filter { $0 == true }
+    .count
+
+print("Part 2: \(result)")
