c6fd12be642872c607fa7cbeaad1b62681fe0042
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-01 21:10:34 +0100

    Use Algorithms

--- swift/day1/Package.swift
+++ swift/day1/Package.swift
@@ -5,12 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "day1",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "day1",
-            dependencies: [],
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms")
+            ],
             resources: [.process("input.txt")]
         ),
         .testTarget(
--- swift/day1/Sources/day1/main.swift
+++ swift/day1/Sources/day1/main.swift
@@ -1,12 +1,13 @@
 import Foundation
+import Algorithms
 
 let fileURL = Bundle.module.url(forResource: "input", withExtension: "txt")
 let input = try! String(contentsOf: fileURL!)
 
 let lines = input.split { $0.isNewline }
-    .map { Int($0) }
+    .map { Int($0)! }
 
-let numberOfIncreases = zip(lines, lines.dropFirst()).map { $0! < $1! }
+let numberOfIncreases = lines.adjacentPairs().map { $0 < $1 }
     .filter { $0 == true }
     .count
 
