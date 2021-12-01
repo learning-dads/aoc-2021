09e3988fe609d41f6b5e4782b2c15cb6b2cdc08d
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-01 17:26:36 +0100

    Load input from bundle

--- swift/day1/Package.swift
+++ swift/day1/Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "day1",
-            dependencies: []
+            dependencies: [],
+            resources: [.process("input.txt")]
         ),
         .testTarget(
             name: "day1Tests",
