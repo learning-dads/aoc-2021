b7ffbcb06858e0f6b2e8405fcb88bf1c8579035e
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-01 17:06:53 +0100

    First iteration w/o file reading

---
+++ swift/day1/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "day1",
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "day1",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "day1Tests",
+            dependencies: ["day1"]),
+    ]
+)
