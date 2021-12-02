09d31b0353a97c031e4ee788ed2e1bf2c87241e9
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-02 11:12:50 +0100

    Add basic structure

---
+++ Tests/aoc-2021Tests/aoc_2021Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import aoc_2021
+
+final class aoc_2021Tests: XCTestCase {
+    func testDay1() {
+        let day = Day1()
+    }
+
+    func testDay2() {
+        let day = Day2()
+    }
+}
