bdf52eacc89cff19c7e1aba6d01caa641cf9eb49
Marc Kalmes <mkalmes@mkalmes.net>
2021-12-02 12:18:40 +0100

    First draft of input reading

--- Sources/aoc-2021/Day 1/Day1.swift
+++ Sources/aoc-2021/Day 1/Day1.swift
@@ -1,3 +1,20 @@
 import Foundation
 
-public struct Day1 {}
+public struct Day1 {
+    let inputs: String
+
+    init() {
+        let root = URL(fileURLWithPath: "\(#file)").deletingLastPathComponent()
+        let enumerator = FileManager.default.enumerator(at: root, includingPropertiesForKeys: nil)
+
+        var inputURL: URL?
+        while let next = enumerator?.nextObject() as? URL {
+            if next.pathComponents.contains("input.txt") == false {
+                continue
+            }
+            inputURL = next
+        }
+
+        inputs = try! String(contentsOf: inputURL!)
+    }
+}
